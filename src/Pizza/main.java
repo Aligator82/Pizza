ce2835dd642183892b56eb691bb74e10813e2002
Aligator <82aligator82@gmail.com>
2017-09-23 17:13:09 +0300

    first pizza commit

---
+++ src/Pizza/main.java
@@ -0,0 +1,15 @@
+package Pizza;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class main {
+    private Scanner file;
+
+    public void openFile (){
+        try {
+            file = new Scanner(new File("D://123.txt"))
+        }catch(){
+    }
+
+}
